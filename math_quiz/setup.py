10721b6ef1f2de4fde37ba853c691f7f2166b0d7
Rutvi1309 <150432713+Rutvi1309@users.noreply.github.com>
2023-11-10 17:25:46 +0100

    Update setup.py

--- math_quiz/setup.py
+++ math_quiz/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='math_quiz',  # Replace with your package name
+    name='dsss_homework_2', 
     version='1.0',      # Replace with your package version
     packages=find_packages(),
     # Add other metadata as required
