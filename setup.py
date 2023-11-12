2f71bd213396fa225a360808c634925d0f4e15ea
rutvishah <rutvi1309shah@gmail.com>
2023-11-12 16:00:18 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['random'],  # Mention dependencies here
+)
