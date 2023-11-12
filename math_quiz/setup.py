4d94398ed621b083e48f7f62a71d3bf0f68ba8f3
Rutvi1309 <150432713+Rutvi1309@users.noreply.github.com>
2023-11-12 16:05:13 +0100

    Update and rename setup.py to random.py

--- math_quiz/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='dsss_homework_2', 
-    version='1.0',      # Replace with your package version
-    packages=find_packages(),
-    # Add other metadata as required
-)
